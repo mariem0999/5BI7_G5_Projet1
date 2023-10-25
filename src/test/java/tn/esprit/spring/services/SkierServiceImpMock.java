9c36b055e0e1e5f5cc62e8f9e24407d3f965b3cb
mhamed-fray <mhamed.fray@esprit.tn>
2023-10-25 14:18:17 +0100

    update partie test 3.2

--- src/test/java/tn/esprit/spring/services/SkierServiceImpMock.java
+++ src/test/java/tn/esprit/spring/services/SkierServiceImpMock.java
@@ -1,20 +1,17 @@
 package tn.esprit.spring.services;
+
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.MethodOrderer.OrderAnnotation;
 import org.junit.jupiter.api.Test;
-import org.junit.jupiter.api.TestMethodOrder;
 import org.junit.jupiter.api.extension.ExtendWith;
-import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.Mockito;
-import org.mockito.junit.MockitoJUnitRunner;
 import org.mockito.junit.jupiter.MockitoExtension;
 import org.springframework.boot.test.context.SpringBootTest;
 import tn.esprit.spring.entities.Piste;
 import tn.esprit.spring.entities.Registration;
-import tn.esprit.spring.repositories.ISkierRepository;
 import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.repositories.ISkierRepository;
 
 import java.time.LocalDate;
 import java.util.*;
@@ -22,29 +19,31 @@ import java.util.*;
 import static org.mockito.Mockito.when;
 
 @SpringBootTest
-@TestMethodOrder(OrderAnnotation.class)
 @ExtendWith(MockitoExtension.class)
-@RunWith(MockitoJUnitRunner.class)
 public class SkierServiceImpMock {
+
     @Mock
-    ISkierRepository   skierRepository;
+    ISkierRepository skierRepository;
+
     @InjectMocks
-     ISkierServices skierServices;
+    ISkierServices skierServices;
+
     Set<Piste> pistes = new HashSet<>();
     Set<Registration> registrations = new HashSet<>();
 
+    Skier skier = new Skier(1L, "fray", "mhamed", LocalDate.of(2020, 5, 15), "nabeul", pistes, registrations);
 
-    Skier skier = new Skier( 1L,"fray","mhamed", LocalDate.of(2020, 5, 15),"nabeul", pistes,registrations);
     List<Skier> skierList = new ArrayList<Skier>() {
         {
-            add(new Skier(12L,"fray1","mhamed1", LocalDate.of(2021, 6, 16),"nabeul1", pistes,registrations));
-            add(new Skier(13L,"fray2","mhamed2", LocalDate.of(2023, 7, 17),"nabeul2", pistes,registrations));
+            add(new Skier(12L, "fray1", "mhamed1", LocalDate.of(2021, 6, 16), "nabeul1", pistes, registrations));
+            add(new Skier(13L, "fray2", "mhamed2", LocalDate.of(2023, 7, 17), "nabeul2", pistes, registrations));
         }
     };
+
     @Test
-    public void retrieveSkier (){
+    public void retrieveSkier() {
         when(skierRepository.findById(Mockito.anyLong())).thenReturn(Optional.of(skier));
-        Skier  skier1 = skierServices.retrieveSkier(Long.valueOf("1L"));
+        Skier skier1 = skierServices.retrieveSkier(1L); // Pass a Long value, not a string
         Assertions.assertNotNull(skier1);
     }
 }
