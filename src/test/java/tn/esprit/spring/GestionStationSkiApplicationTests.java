c4d2d87efb2ad65c1d3cdb279320a27acfdcdc48
mhamed-fray <mhamed.fray@esprit.tn>
2023-10-25 12:57:49 +0100

    update partie test 2

--- src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
+++ src/test/java/tn/esprit/spring/GestionStationSkiApplicationTests.java
@@ -1,12 +1,19 @@
 package tn.esprit.spring;
 
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Order;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+import tn.esprit.spring.entities.Skier;
+import tn.esprit.spring.services.ISkierServices;
+
+import java.util.List;
 
 @SpringBootTest
 class GestionStationSkiApplicationTests {
 
-
+	@Test
 	void contextLoads() {
 	}
 
