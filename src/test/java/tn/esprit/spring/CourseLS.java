1b07ed845c7e9075c96482d3a30f978eb9377c64
Lobna SAKLI <lobna.sakli@esprit.tn>
2023-11-04 20:10:59 +0100

    test classe courseLS

---
+++ src/test/java/tn/esprit/spring/CourseLS.java
@@ -0,0 +1,88 @@
+package tn.esprit.spring;
+
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+import tn.esprit.spring.entities.Course;
+import tn.esprit.spring.repositories.ICourseRepository;
+import tn.esprit.spring.services.CourseServicesImpl;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+
+public class CourseLS {
+
+    @InjectMocks
+    private CourseServicesImpl courseService;
+
+    @Mock
+    private ICourseRepository courseRepository;
+
+    @Before
+    public void init() {
+        MockitoAnnotations.initMocks(this);
+    }
+
+    @Test
+    public void testRetrieveAllCourses() {
+        // Créez une liste factice de cours
+        List<Course> courseList = new ArrayList<>();
+        courseList.add(new Course());
+        courseList.add(new Course());
+
+        when(courseRepository.findAll()).thenReturn(courseList);
+
+        List<Course> result = courseService.retrieveAllCourses();
+
+        Assert.assertEquals(2, result.size());
+    }
+
+    @Test
+    public void testAddCourse() {
+        Course course = new Course();
+
+        when(courseRepository.save(course)).thenReturn(course);
+
+        Course result = courseService.addCourse(course);
+
+        Assert.assertNotNull(result);
+    }
+
+    @Test
+    public void testUpdateCourse() {
+        Course course = new Course();
+
+        when(courseRepository.save(course)).thenReturn(course);
+
+        Course result = courseService.updateCourse(course);
+
+        Assert.assertNotNull(result);
+    }
+
+ /*   @Test
+    public void testDeleteCourse() {
+        Long courseNum = 1L;
+
+        courseService.retrieveCourse(courseNum);
+
+        verify(courseRepository).deleteById(courseNum);
+    }
+*/
+    @Test
+    public void testRetrieveCourse() {
+        Long courseNum = 1L;
+        Course course = new Course();
+        when(courseRepository.findById(courseNum)).thenReturn(Optional.of(course));
+
+        Course result = courseService.retrieveCourse(courseNum);
+
+        Assert.assertNotNull(result);
+    }
+}
